3e2da4957a0d641c1bad8bd9b9d13f2d0835f303
Jason Bodman <32904662+jasonbodman@users.noreply.github.com>
2021-08-23 17:03:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const app = express();
 //and if PORT is undefined, use 8081
 const port = process.env.PORT || 8081;
 
+function setText(f, l) {
+	var full;
+	if (f == undefined || l == undefined) {
+		full = "Valued Customer1"
+	} else {
+		full = f + ' ' + l
+		if (full.length >= 20) {
+			full = "Valued Customer2"
+		}
+	}
+}
+
 const makeMeme = async ({
     //the text to put on the image
     url,
@@ -22,7 +34,7 @@ const makeMeme = async ({
     //if there's no image to work with
     //don't try anything
     
-    const input = fname + ' ' + lname;
+    const input = setText(fname, lname);
     const canvas = createCanvas(200, 200);
     const context = canvas.getContext("2d");
   
