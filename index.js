59c80552138ed79be68ae36737c6d3eef1e308e8
Jason Bodman <32904662+jasonbodman@users.noreply.github.com>
2021-08-23 16:05:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,51 +66,6 @@ const makeMeme = async ({
     return canvas.toBuffer();
   };
 
-//accepts an input string
-//returns an image of the input text as a buffer
-const makeTextImage = (input) => {
-  //creates the html canvas object
-  //with a width of 200px
-  //and a height of 200px
-  const canvas = createCanvas(200, 200);
-
-  //a reference to the 2d canvas rendering context
-  //used for drawing shapes, text, and images
-  const context = canvas.getContext("2d");
-
-  //the font we are using
-  const fontSetting = "bold 50px sans-serif";
-
-  //set context to use the fontSetting
-  context.font = fontSetting;
-
-  //context.measureText is a function that measures the text
-  //so we can adjust how wide the finished image is
-  const textWidth = context.measureText(input).width;
-
-  //change the canvas width to be wider than the text width
-  canvas.width = textWidth + 100;
-
-  //changing canvas width resets the canvas, so change the font again
-  context.font = fontSetting;
-
-  //fillStyle sets the color that you are drawing onto the canvas
-  context.fillStyle = "white";
-
-  //fillText draws text onto the canvas
-  context.fillText(input, 50, 50, textWidth + 50);
-
-  //set the color to black for the outline
-  context.fillStyle = "black";
-
-  //strokeText draws an outline of text on the canvas
-  context.strokeText(input, 50, 50, textWidth + 50);
-
-  //return a buffer (binary data) instead of the image itself
-  return canvas.toBuffer();
-};
-
-
 //ROUTES
 //this is a 'route'
 //it defines the response to an http 'get' request
@@ -129,69 +84,6 @@ app.get("/campaigns/:job/:name", async (req, res) => {
  	res.end(finalImage);
 	
 })
-
-//this route has two parameters
-//input is a string
-//url* matches everything after input
-app.get("/meme/:input/:url*", async (req, res) => {
-    const { params } = req;
-    //get the text input string from the request parameters
-    const input = params?.input;
-  
-  
-    //urls have '/' characters in them
-    //but '/' is what express uses to divide up route parameters
-    //so to match the whole url, we use an asterisk '*'
-    //the asterisk matches everything after the first '/'
-    //and assigns it to params[0]
-    
-    //so params.url will usually be http:
-    const baseUrl = params?.url;
-    //and params[0] will be www.myImageHost.com/image.jpg
-    const restOfUrl = params?.[0];
-  
-    //put the baseUrl and restOfUrl together
-    const url = baseUrl + restOfUrl;
-  
-    //get the image buffer
-    const image = await makeMeme({ url, input });
-  
-    //create headers object
-    const headers = { "Content-Type": "image/jpg" };
-  
-    //set status code and headers
-    res.writeHead(200, headers);
-  
-    //end by sending image
-    res.end(image);
-  });
-
-//text is the route
-//:input designates a parameter of the route
-app.get("/text/:input", (req, res) => {
-    //the ? means optional chaining
-    //input will be a string equal to whatever the user types after the route
-    const input = req?.params?.input;
-  
-    //call the makeTextImage function
-    //and wait for it to return the buffer object
-    const image = makeTextImage(input);
-  
-    //create the headers for the response
-    //200 is HTTTP status code 'ok'
-    res.writeHead(
-      200,
-      //this is the headers object
-      {
-        //content-type: image/jpg tells the browser to expect an image
-        "Content-Type": "image/jpg",
-      }
-    );
-  
-    //ending the response by sending the image buffer to the browser
-    res.end(image);
-  });
-  
 
 //start the web server listening
 app.listen(port, () => {
